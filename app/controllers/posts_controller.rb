c46e9b9ab91683da6bdd71158cd1c358ad221b07
Adler <nkj20932@hotmail.com>
2015-04-26 01:49:27 +0800

    add post parsing markdown

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,35 +1,7 @@
 class PostsController < ApplicationController
-  require 'redcarpet'
-  require 'coderay'
+  before_action :set_post, :only => [:show]
+
   def index
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true)
-    options = {
-      :line_numbers => :table,
-      :line_number_anchors => false,
-      :css => :class
-    }
-    # rendered = markdown.render(File.read(File.expand_path("../red.rb", __FILE__)))
-    raw = markdown.render(File.read(File.expand_path("../red.rb", __FILE__)))
-    while index = raw =~ /<pre>/
-      @content ||= ""
-      @content += raw[0..index+4]
-      raw = raw[index+5..-1]
-      end_index = raw =~ />/
-      @content += raw[0..end_index]
-      raw = raw[end_index+1..-1]
-      end_block_index = raw =~ /<\/code>/
-      block = CodeRay.scan(raw[0...end_block_index], :ruby).html(options)   
-      @content += block
-      raw = raw[end_block_index-1..-1]
-      end_block_end_index = raw =~ /<\/pre>/
-      @content += raw[0..end_block_end_index+5]
-      raw = raw[end_block_end_index+6..-1]
-    end
-    @content += raw
-    @content.gsub!("&amp;quot;","&quot;") # 去除雙括號
-    @content.gsub!('&amp;<span class="comment">#39;',"&#39;") # 去除單括號(Coderay誤認為註解)
-    @content.gsub!('&amp;#39;</span>',"&#39;") # 去除單括號(Coderay誤認為註解的結尾)
-    @content.gsub!('&amp;#39;',"&#39;") # 去除其他單括號
   end
 
   def new
@@ -40,4 +12,9 @@ class PostsController < ApplicationController
 
   def show
   end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,42 @@
 class Post < ActiveRecord::Base
+  require 'redcarpet'
+  require 'coderay'
+
+  def parse
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true)
+    options = {
+      :line_numbers => :table,
+      :line_number_anchors => false,
+      :css => :class
+    }
+    # raw = markdown.render(File.read(File.expand_path("../red.rb", __FILE__)))
+    raw = markdown.render(content)
+    while index = raw =~ /<pre>/
+      @content ||= ""
+      @content += raw[0..index+4]
+      raw = raw[index+5..-1]
+      end_index = raw =~ />/
+      code_class = raw[0..end_index]
+      [:ruby, :cmd, :javascript].each do |lang|
+        @language = lang if code_class.index(lang.to_s)
+      end
+      @language ||= :html
+      @content += raw[0..end_index]
+      raw = raw[end_index+1..-1]
+      end_block_index = raw =~ /<\/code>/
+      block = CodeRay.scan(raw[0...end_block_index], @language).html(options)   
+      @content += block
+      raw = raw[end_block_index-1..-1]
+      end_block_end_index = raw =~ /<\/pre>/
+      @content += raw[0..end_block_end_index+5]
+      raw = raw[end_block_end_index+6..-1]
+    end
+    @content += raw
+    @content.gsub!("&amp;quot;","&quot;") # 去除雙括號
+    @content.gsub!('&amp;<span class="comment">#39;',"&#39;") # 去除單括號(Coderay誤認為註解)
+    @content.gsub!('&amp;#39;</span>',"&#39;") # 去除單括號(Coderay誤認為註解的結尾)
+    @content.gsub!('&amp;#39;',"&#39;") # 去除其他單括號
+    return @content.html_safe
+  end
+
 end
