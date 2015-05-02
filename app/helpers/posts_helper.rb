ea794d815b26ffe941cfbbf704cb83873500af59
Adler <nkj20932@hotmail.com>
2015-05-02 23:01:39 +0800

    customise blog title

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def render_blog_title(custom=nil)
+    if custom
+      content_tag(:h3, custom, class: "blog-title")
+    else
+      content_tag(:h3, "Blog", class: "blog-title")
+    end
+  end
 end
