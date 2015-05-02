38b649164c787a67454bde22b40e6f0af9689e70
Adler <nkj20932@hotmail.com>
2015-05-02 23:30:13 +0800

    add users#show routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users 
+  devise_for :users
   as :user do
     get '/sign_in', to: "devise/sessions#new", as: "sign_in"
     get '/sign_out', to: "devise/sessions#destroy", as: "sign_out"
