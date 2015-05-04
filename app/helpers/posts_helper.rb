19a705617655019df5174fc5261cef40cc25a23c
Adler <nkj20932@hotmail.com>
2015-05-04 21:48:46 +0800

    make render author automatically

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,7 @@
 module PostsHelper
+  def render_author(text)
+    link_to( text, author_path )
+  end
   def render_blog_title(custom=nil)
     if custom
       content_tag(:h3, custom, class: "blog-title")
