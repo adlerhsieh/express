968215eddf1f6db8aa2a5435c88a1ca26900766d
Adler <nkj20932@hotmail.com>
2015-05-05 22:18:06 +0800

    move tab switch class check to helper

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,22 +1,22 @@
-var use_default_tab = true;
-
-$(document).ready(function(){
-  if(location.href.indexOf("users") != -1){
-    tab_switch("posts");
-    tab_switch("categories");
-    default_tab();
-  };
-});
-
-function tab_switch(tab) {
-  if(location.href.indexOf(tab) != -1){
-    $("#" + tab).addClass("active");
-    use_default_tab = false;
-  };
-};
-
-function default_tab() {
-  if(use_default_tab == true){
-    $("#basic_info").addClass("active");
-  };
-};
+// var use_default_tab = true;
+//
+// $(document).ready(function(){
+//   if(location.href.indexOf("users") != -1){
+//     tab_switch("posts");
+//     tab_switch("categories");
+//     default_tab();
+//   };
+// });
+//
+// function tab_switch(tab) {
+//   if(location.href.indexOf(tab) != -1){
+//     $("#" + tab).addClass("active");
+//     use_default_tab = false;
+//   };
+// };
+//
+// function default_tab() {
+//   if(use_default_tab == true){
+//     $("#basic_info").addClass("active");
+//   };
+// };
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def route_is?(action)
+    if current_page?(:action => action)
+      "active"
+    end
+  end
 end
