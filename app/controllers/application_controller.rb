a2b9c27eba7f28256fad87045d72e26f965b1ff9
Adler <nkj20932@hotmail.com>
2015-05-10 00:05:28 +0800

    refactor setting load into helper and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_filter :configure_devise_params, if: :devise_controller?
+  before_filter :load_settings
   def configure_devise_params
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:name, :email, :password, :password_confirmation)
@@ -21,6 +21,10 @@ class ApplicationController < ActionController::Base
       end
     end
     redirect_to sign_in_path 
+  end
+
+  def load_settings
+    @settings = Setting.all.map(&:serializable_hash)
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,13 @@
 module ApplicationHelper
+
   def is_admin?
     if current_user
       return current_user.is_admin?
     end
     false
+  end
+
+  def set(setting)
+    @settings.find{|s|s["key"] == setting.to_s}["value"]
   end
 end
