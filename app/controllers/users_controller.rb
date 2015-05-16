37895221f51227f40d4d22768823fe37a880241a
Adler <nkj20932@hotmail.com>
2015-05-16 15:37:46 +0800

    add email validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   layout "backend"
-  before_action :require_login
+  before_action :require_login, :setup
+  before_action :set_email, only: [:subscribe]
   # require 'rest-client'
 
   def show
@@ -8,9 +9,39 @@ class UsersController < ApplicationController
   end
 
   def subscribe
-    email = params[:email]
-    @mailgun = Mailgun()
-    # puts @mailgun.list_members("service@mg.motion-express.com").list
-    redirect_to posts_path
+    if email_formatted? && email_valid?
+      list = "service@mg.motion-express.com" 
+      @mailgun.list_members(list).add(@email)
+      flash[:notice] = "已將#{email}加入名單"
+      redirect_to posts_path
+    end
   end
+
+  private
+    def setup
+      @mailgun = Mailgun()
+    end
+
+    def set_email
+      @email = params[:email]
+    end
+
+    def email_valid?
+      list = @mailgun.list_members("service@mg.motion-express.com").list.map{|m|m["address"]}
+      if list.include? @email
+        flash[:alert] = "email已經存在於名單內"
+        redirect_to posts_path
+        return false
+      end
+      true
+    end
+
+    def email_formatted?
+      if not @email.include?("@")
+        flash[:alert] = "請輸入正確的email格式"
+        redirect_to posts_path
+        return false
+      end
+      true
+    end
 end
