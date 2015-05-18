9da7d459d356a60e34b5cef070547490b93bf0bb
Adler <nkj20932@hotmail.com>
2015-05-18 23:43:26 +0800

    make email title dynamic

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,9 +1,14 @@
 class SubscriptionMailer < ApplicationMailer
   default from: "service@motion-express.com"
 
-  def subscription(email,item)
-    @item = item
-    mail(to: email, subject: "測試Email") do |format|
+  def subscription(email,items)
+    @items = items
+    if @items.length > 1
+      title = "特快車更新：#{@items.first.title}及其他文章"
+    else
+      title = "特快車更新：#{@items.first.title}"
+    end
+    mail(to: email, subject: title) do |format|
       format.text
       format.html
     end
