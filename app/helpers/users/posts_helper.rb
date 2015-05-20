d654d06559f8ff51d44e08a16522bcb8fd57b26a
Adler <nkj20932@hotmail.com>
2015-05-20 13:59:05 +0800

    debug post.sent display

--- app/helpers/users/posts_helper.rb
+++ app/helpers/users/posts_helper.rb
@@ -1,7 +1,7 @@
 module Users::PostsHelper
   def mail_link(post)
     path = send_post_email_user_path(current_user["name"],post[:id])
-    if post.sent
+    if post.sent == true
       link_to "Email訂閱已發", path, method: :post, data: { confirm: "確定再次發送？" }, class: "btn btn-default"
     else
       link_to "發送Email訂閱", path, method: :post, data: { confirm: "確定發送？" }, class: "btn btn-success"
