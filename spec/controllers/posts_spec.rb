d6a00c736ecac01ee20b54432827dd0cd794e4f6
Adler <nkj20932@hotmail.com>
2015-06-13 12:52:41 +0800

    update RSpec

--- spec/controllers/posts_spec.rb
+++ spec/controllers/posts_spec.rb
@@ -6,17 +6,24 @@ describe PostsController, type: :controller do
   let(:private_post){create(:post, :private)}
   before do
     sign_in user 
+    allow(controller).to receive(:load_settings).and_return(nil)
   end
 
   describe "#index" do
     before { get :index }
     it { expect(response).to have_http_status(200) }
     it { expect(response).to render_template(:index) }
+    # it { expect(response.body).to include(public_post.title) }
+    # it { expect(response.body).not_to include(private_post.title) }
   end
 
-  specify "#new" do
-    get :new
-    expect(response).to have_http_status(200)
-    expect(response).to render_template(:new)
+  describe "#show" do
+    before { get :show, slug: public_post.slug }
+    it { expect(response).to have_http_status(200) }
   end
+  # specify "#new" do
+  #   get :new
+  #   expect(response).to have_http_status(200)
+  #   expect(response).to render_template(:new)
+  # end
 end
