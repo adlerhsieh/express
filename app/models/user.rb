768097f429f68038f7612e8f37bd909e3d04adf2
Adler <nkj20932@hotmail.com>
2015-06-15 01:10:53 +0800

    add user.cart method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:github]
+  has_many :orders, :class_name => "Store::Order"
   require 'bing_translator'
+
+  def cart
+    @cart ||= Store::Order.where(:aasm_state => "cart").order(:updated_at => :desc).first
+    if not @cart
+      @cart = Store::Order.new
+      @cart.save!
+    end
+    return @cart
+  end
 
   def is_admin?
     is_admin
