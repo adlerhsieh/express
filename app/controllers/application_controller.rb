9f1505835e24c0714b33e88afdaf07ba53ef9c3e
Adler <nkj20932@hotmail.com>
2015-06-15 01:10:39 +0800

    add other authenticate option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
   before_filter :configure_devise_params, if: :devise_controller?
   before_filter :load_settings
 
