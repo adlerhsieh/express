d38c21eff9dd6288d8656d1a5d3293d35d815385
Adler <nkj20932@hotmail.com>
2015-06-15 01:01:25 +0800

    add CSRF-token to ajax

--- app/assets/javascripts/frontend/store/products.js
+++ app/assets/javascripts/frontend/store/products.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
   var p_id;
   $(".add-to-cart").click(function(){
     p_id = $(this).attr("id").replace("p-","");
+    token = $("meta[name='csrf-token']").attr("content");
     $.ajax({
       url: "/store/orders/" + p_id + "/add_to_cart",
       method: "POST",
-      data: {}
+      data: {'CSRFToken': token}
     }).success(function(response){
       console.log(response);
     });
