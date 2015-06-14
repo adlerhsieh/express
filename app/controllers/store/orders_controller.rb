ed29d0abd1611976585481c7f7991790930e875d
Adler <nkj20932@hotmail.com>
2015-06-15 01:10:19 +0800

    add action to orders controller

--- app/controllers/store/orders_controller.rb
+++ app/controllers/store/orders_controller.rb
@@ -1,5 +1,11 @@
 class Store::OrdersController < ApplicationController
+  before_action :require_login
+  skip_before_filter :verify_authenticity_token, only: [:add_to_cart]
+  # before_filter :authenticate_user!
+
   def add_to_cart
-    render json: "success"
+    cart = current_user.cart
+    result = cart.add_item(params[:id])
+    render json: "已加入購物車：#{result[:title]}，數量#{result[:quantity]}，總價#{result[:price]}"
   end
 end
