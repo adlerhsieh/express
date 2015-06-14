8ffbce9d9ae0d3284867383ab0e6abb72814a245
Adler <nkj20932@hotmail.com>
2015-06-15 01:01:15 +0800

    add action and relation to order

--- app/models/store/order.rb
+++ app/models/store/order.rb
@@ -1,7 +1,34 @@
 class Store::Order < ActiveRecord::Base
+  belongs_to :user
+  has_many :items, :class_name => "Store::OrderItem", :foreign_key => "order_id"
   include AASM
+  before_save :generate_token, :set_paid_false
+
+  def generate_token
+    self.token = SecureRandom.hex if not self.token
+  end
+
+  def set_paid_false
+    self.paid = false if self.paid.nil?
+  end
+
+  def add_item(product_id)
+    item = self.items.find_by_product_id(product_id)
+    if item
+      quantity = item.quantity
+      item.update_column(:quantity, quantity + 1)
+    else
+      product = Store::Product.find(product_id)
+      item = self.items.create(:product_id => product.id, 
+                        :quantity => 1,
+                        :price => product.price
+                       )
+    end
+    {title: item.product.title, quantity: item.quantity, price: item.price}
+  end
 
   aasm do
+    state :outdated
     state :cart, :initial => true
     state :placed
     state :paid
@@ -10,6 +37,9 @@ class Store::Order < ActiveRecord::Base
     state :cancelled
     state :returned
 
+    event :outdate do
+      transitions from: [:cart, :placed], to: :outdated
+    end
     event :checkout do
       transitions from: :cart, to: :placed
     end
--- app/models/store/order_item.rb
+++ app/models/store/order_item.rb
@@ -1,2 +1,4 @@
 class Store::OrderItem < ActiveRecord::Base
+  belongs_to :order, :class_name => "Store::Order", :foreign_key => "order_id"
+  belongs_to :product, :class_name => "Store::Product", :foreign_key => "product_id"
 end
