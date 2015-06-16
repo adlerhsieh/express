712cb379cc54fb6c36a8a8d61098d66391c1aa2c
Adler <nkj20932@hotmail.com>
2015-06-17 01:03:46 +0800

    add form to orders#show page

--- app/controllers/store/orders_controller.rb
+++ app/controllers/store/orders_controller.rb
@@ -2,6 +2,6 @@ class Store::OrdersController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:add_to_cart]
   # before_filter :authenticate_user!
   def show
-    @order = Store::Order.find_by_token(params[:id])
+    @order = Store::Order.includes(:items => [:product]).find_by_token(params[:id])
   end
 end
