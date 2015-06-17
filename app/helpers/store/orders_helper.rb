22c63f9f8446629541c10d7c1175f14cb85959a4
Adler <nkj20932@hotmail.com>
2015-06-17 21:38:36 +0800

    add progress bar and status in orders#show

---
+++ app/helpers/store/orders_helper.rb
@@ -0,0 +1,45 @@
+module Store::OrdersHelper
+  def order_title
+    if @order.aasm_state == "cart"
+      content_tag(:h2, "購物車明細")
+    else
+      content_tag(:h2, "訂單明細")
+    end
+  end
+
+  def status_print(state)
+    case state
+    when "cart"
+      "購物車"
+    when "placed"
+      "待結帳"
+    when "paid"
+      "已付款"
+    when "shipped"
+      "已出貨"
+    when "arrived"
+      "已到貨"
+    when "cancelled"
+      "已取消"
+    when "returned"
+      "已退回"
+    when "outdated"
+      "已過期"
+    end
+  end
+
+  def progress_bar
+    return if @order.aasm_state == "cart"
+    value = case @order.aasm_state
+    when "placed"
+      "10"
+    when "paid"
+      "50"
+    when "delivered"
+      "100"
+    else
+      "0"
+    end
+    content_tag(:progress, "", :max => "100", :value => value)
+  end
+end
