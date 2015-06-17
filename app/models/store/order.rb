a9322b6fba89bf57e395f33de57e91209bc517ab
Adler <nkj20932@hotmail.com>
2015-06-17 23:10:13 +0800

    add relation to order and info

--- app/models/store/order.rb
+++ app/models/store/order.rb
@@ -1,6 +1,7 @@
 class Store::Order < ActiveRecord::Base
   belongs_to :user
   has_many :items, :class_name => "Store::OrderItem", :foreign_key => "order_id"
+  has_one :info, :class_name => "Store::OrderInfo", :foreign_key => "order_id"
   include AASM
   extend FriendlyId
   friendly_id :token
