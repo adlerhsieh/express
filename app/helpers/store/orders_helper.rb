a3982643483791690a13a06ff396371c4115704a
Adler <nkj20932@hotmail.com>
2015-06-17 22:54:04 +0800

    organise action buttons to helpers

--- app/helpers/store/orders_helper.rb
+++ app/helpers/store/orders_helper.rb
@@ -43,6 +43,15 @@ module Store::OrdersHelper
     content_tag(:progress, "", :max => "100", :value => value)
   end
 
+  def progress_bar_status
+    return if @order.aasm_state == "cart"
+    content_tag(:div,
+               content_tag(:span, "下單", id: "place") + 
+               content_tag(:span, "付款", id: "pay") + 
+               content_tag(:span, "出貨", id: "deliver"),
+               :class => "status-bar")
+  end
+
   def check_stock(item)
     quantity = item.quantity
     stock = item.product.stock
@@ -50,6 +59,18 @@ module Store::OrdersHelper
       content_tag(:span, "不足", style: "color: red")
     else
       content_tag(:span, "有")
+    end
+  end
+
+  def action_button
+    _class = "btn btn-primary action"
+    case @order.aasm_state
+    when "cart"
+      link_to("確認下單", place_store_order_path(@order), class: _class)
+    when "placed"
+      link_to("前往付款頁面", "", class: _class)
+    else
+      ""
     end
   end
 end
