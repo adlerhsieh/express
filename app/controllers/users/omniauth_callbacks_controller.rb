7f6e74a6e2097aeaa942ac3d89118dbddbae8539
Adler <nkj20932@hotmail.com>
2015-06-22 21:18:26 +0800

    debug github login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,31 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
-    else
-      # session["devise.github_data"] = request.env["omniauth.auth"]
-      flash[:alert] = "登入失敗"
-      redirect_to posts_path
+    @auth = request.env["omniauth.auth"]
+    user = User.find_by_omniauth(auth_params)
+    if not user
+      user = User.create!(auth_params)
     end
+    # if user.persisted?
+    sign_in_and_redirect user, :event => :authentication
+    set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
+    # else
+      # session["devise.github_data"] = request.env["omniauth.auth"]
+      # flash[:alert] = "登入失敗"
+      # redirect_to posts_path
+    # end
+  end
+
+  private
+
+  def auth_params
+    token = Devise.friendly_token[0,20]
+    {
+      email: @auth.info.email,
+      password: token,
+      password_confirmation: token,
+      name: @auth.info.name,
+      provider: @auth.provider,
+      uid: @auth.uid
+    }
   end
 end
