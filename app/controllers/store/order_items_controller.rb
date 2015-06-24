c02f6364db64a02c967bc4fb286daafc4d502975
Adler <nkj20932@hotmail.com>
2015-06-24 22:58:13 +0800

    update order total price after deleting order_item

--- app/controllers/store/order_items_controller.rb
+++ app/controllers/store/order_items_controller.rb
@@ -3,6 +3,7 @@ class Store::OrderItemsController < ApplicationController
     item = Store::OrderItem.find(params[:id])
     order = item.order
     item.delete
+    order.update_total_price
     flash[:notice] = "已刪除：#{item.product.title}"
     redirect_to store_order_path(order)
   end
