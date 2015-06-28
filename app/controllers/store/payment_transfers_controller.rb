7d5490749f090779d494504f91f46cd14e354bcf
Adler <nkj20932@hotmail.com>
2015-06-29 00:30:57 +0800

    add transfer edit

--- app/controllers/store/payment_transfers_controller.rb
+++ app/controllers/store/payment_transfers_controller.rb
@@ -24,7 +24,25 @@ class Store::PaymentTransfersController < ApplicationController
   end
 
   def update
-    
+    @transfer = Store::PaymentTransfer.find_by_order_id(@order.id)
+    t = @transfer.transaction_id
+    if @transfer.update!(transfer_params) && t.to_i.to_s.length == 5
+      flash[:notice] = "轉帳資訊更新成功"
+      redirect_to store_order_path(@order)
+    else
+      flash[:alert] = "請確認已填入轉帳帳號末五碼"
+      render :edit
+    end
+  end
+
+  def destroy
+    @transfer = Store::PaymentTransfer.find_by_order_id(@order.id)
+    if @transfer
+      @transfer.delete
+      @order.cancel_transfer!
+      flash[:notice] = "刪除成功"
+      redirect_to store_order_path(@order)
+    end
   end
 
   private
