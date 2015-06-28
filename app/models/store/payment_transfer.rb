2eef89561f81b8c18f66b2fe40b33dd8d7a44941
Adler <nkj20932@hotmail.com>
2015-06-28 13:30:09 +0800

    add default values to payment_transfer

--- app/models/store/payment_transfer.rb
+++ app/models/store/payment_transfer.rb
@@ -1,10 +1,11 @@
 class Store::PaymentTransfer < ActiveRecord::Base
   belongs_to :order, :class_name => "Store::Order", :foreign_key => "order_id"
-  # before_create :default_values
+  before_create :default_values
   validates_presence_of :transaction_id
 
   def default_values
     self.status = "Pending" unless self.status
     self.confirm = false unless self.confirm
+    return true
   end
 end
