b08466f25e4499ed19aa7956e3ec858eff744fde
Adler <nkj20932@hotmail.com>
2015-06-28 13:09:27 +0800

    add payment_transfer controller and view

--- app/controllers/store/payment_transfers_controller.rb
+++ app/controllers/store/payment_transfers_controller.rb
@@ -6,11 +6,30 @@ class Store::PaymentTransfersController < ApplicationController
   end
 
   def create
+    @transfer = @order.transfers.new(transfer_params)
+    if @transfer.save
+      flash[:notice] = "已送出轉帳資訊"
+      redirect_to store_order_path(@order)
+    else
+      flash[:alert] = "請確認已填入轉帳帳號末五碼"
+      render :new
+    end
+  end
+
+  def edit
+    @transfer = @order.transfers.last
+  end
+
+  def update
     
   end
 
   private
     def set_order
       @order = Store::Order.find_by_token(params[:order_id])
+    end
+
+    def transfer_params
+      params.require(:store_payment_transfer).permit(:transaction_id)
     end
 end
--- app/models/store/payment_transfer.rb
+++ app/models/store/payment_transfer.rb
@@ -1,3 +1,10 @@
 class Store::PaymentTransfer < ActiveRecord::Base
   belongs_to :order, :class_name => "Store::Order", :foreign_key => "order_id"
+  # before_create :default_values
+  validates_presence_of :transaction_id
+
+  def default_values
+    self.status = "Pending" unless self.status
+    self.confirm = false unless self.confirm
+  end
 end
