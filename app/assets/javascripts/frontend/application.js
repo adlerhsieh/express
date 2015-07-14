474597322d47d7c34d36a9ec91348edfdf87bf82
Adler <nkj20932@hotmail.com>
2015-07-15 00:39:10 +0800

    add mobile view to order

--- app/assets/javascripts/frontend/application.js
+++ app/assets/javascripts/frontend/application.js
@@ -28,3 +28,10 @@
 function dismiss_message() {
   $(".message").hide();
 };
+
+$(document).ready(function(){
+  if($(window).width() < 992){
+    $(".pull-right").removeClass("pull-right");
+    $(".product").css("width",null);
+  };
+});
