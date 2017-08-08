882ebbf65fa1c5538bda79b6770a6b2c0a413c58
Adler <nkj20932@hotmail.com>
2017-08-08 23:16:43 +1000

    remove translator callback

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,5 +2,5 @@ class Setting < ActiveRecord::Base
   translates :value
   default_scope {includes(:translations)}
   include DefaultSetter
-  after_save :translate_CN
+  # after_save :translate_CN
 end
