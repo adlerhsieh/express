8ed66fe0563d8a63461899cac9c4128f9a80e500
Adler <nkj20932@hotmail.com>
2018-04-12 22:56:21 +1000

    remove category from translations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,11 @@
 class Category < ActiveRecord::Base
+
+  include DefaultSetter
+
   has_many :posts
   has_many :screencasts
   has_many :trainings
-  translates :name
-  default_scope {includes(:translations)}
-  include DefaultSetter
-  after_save :translate_CN
+
   scope :from_posts, -> {
     all.inject([]){|array, c|
       if c.posts.count > 0
@@ -15,4 +15,5 @@ class Category < ActiveRecord::Base
       end
     }
   }
+
 end
